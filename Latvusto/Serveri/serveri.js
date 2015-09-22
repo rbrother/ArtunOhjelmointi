688da044851b01bdc2dedd55fd34bc023de1218e
Robert Brotherus <robert@iki.fi>
2015-09-22 20:29:43 +0300

    ServerinOnTarkoitusTallentaaSaamansaMerkittävätKoordinaatit+Kotitehtävää

--- Latvusto/Serveri/serveri.js
+++ Latvusto/Serveri/serveri.js
@@ -31,12 +31,14 @@ function respondToOptions( response ) {
 
 function respondToPost(data, response) {
 	console.log(data);
+	console.log("Vaaka on " + data.Vaaka);
+	pelaajienKoordinaatit = data;
 	var headers = {};
 	headers["Access-Control-Allow-Origin"] = "*";
 	headers["Access-Control-Allow-Credentials"] = false;
 	headers["Content-Type"] = 'text/plain';
-	response.writeHead(200, headers);
-	response.write("Onnistu");
+	response.writeHead(200, headers);   
+	response.write( querystring.stringify(pelaajienKoordinaatit) );
 	response.end();
 }
 
@@ -54,3 +56,16 @@ function respondToIncomingMessage(request, response) {
 http.createServer(respondToIncomingMessage).listen(1337,'127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+
+// Kotitehtävä 2015-09-22:
+// - Clientti lähettää tiheästi viestejä serverille nytkin
+//   mutta viesti sisältää ukon koordinaatit vain jos ukko
+//   on liikkunut edellisen viestin jälkeen.
+// - Serveri tallentaa ukon koorinaatit saamastaan viestistä
+//   vain jos viesti sisältää ukon koordinaatit.
+// Jatkoa:
+// - Clientti parsii serveriltä saamansa ukon koordinaatit
+//   javascript-objektiksi.
+// - Clientti päivivvää ukon sijannin, jos serveriltä tulee
+//   uudet koordinaatit, jotka ovat erilaiset kuin nykyiset.
