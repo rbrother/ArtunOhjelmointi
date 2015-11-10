a573e6e19d74dd2d8bb61912c6c9256aa8762fda
Robert Brotherus <robert@iki.fi>
2015-11-10 20:47:31 +0200

    koodiaSiirrettyKenttä.js:ään

---
+++ Latvusto/kentta.js
@@ -0,0 +1,40 @@
+function makeStage(){
+    var y = 0;
+    kentta2.forEach(function(rivi){
+        y++;
+        for(x = 0;x < rivi.length;x++){
+            var merkki = rivi.charAt(x);
+            if(merkki == 'X') { BlokkiaLisaa(x,y); }
+        }
+    });
+}
+
+function BlokkiaLisaa(x,y) {
+    var bitmap = new createjs.Bitmap("Gimp/150x150/0002 150_TestihuoneenMateriaaliPng.png");
+    bitmap.scaleX = skaalaus;
+    bitmap.scaleY = skaalaus;
+    bitmap.x = x*blokinKoko;
+    bitmap.y = y*blokinKoko;
+    stage.addChild(bitmap);
+    blokkienTiedot.push( { x: x*blokinKoko, y: y*blokinKoko } );
+
+    // blokkienTiedot = [ {x: x, y : y}, {x: x, y : y}, {x: x, y : y}, ... ] 
+}
+
+var kentta2 = [
+   '..........X...................',
+   '..........X...................',
+   '..........X...................',
+   '..........X...................',
+   'XXX.......X...................',
+   '..XXXXX...X...................',
+   '..........X...................',
+   '..........X.....X.............',
+   '...XXX....X...................',
+   '.....XXXXXXX..................',
+   '..............................',
+   '..............................',
+   'X........X......X.............',
+   '...X.....X....................',
+   '...XX....XXX..................',
+];
